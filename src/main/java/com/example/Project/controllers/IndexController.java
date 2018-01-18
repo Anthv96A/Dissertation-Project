f0b78ccb3b8a1b5c6441545f32f3b7a4a5538e46
Anthv96A <anthv73@googlemail.com>
2018-01-18 18:30:30 +0000

    Second commit

---
+++ src/main/java/com/example/Project/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.Project.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin({"http://localhost:8100","file://"})
+public class IndexController {
+
+    public String index(){
+        return "Sports Performance";
+    }
+}
